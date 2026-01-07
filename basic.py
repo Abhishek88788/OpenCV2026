7febce13d2225ab4b349bc95c3800da9181a466f
abhishek88788 <abhisheknagar342@gmail.com>
2026-01-08 02:55:39 +0530

    drowshapes

--- basic.py
+++ basic.py
@@ -3,6 +3,11 @@ import cv2
 img = cv2.imread("images\wallpaper-imgs\yasuke-assassins-3840x2160-16782.jpeg")
 
 
+
 resized_img = cv2.resize(img,(256, 144))
-cv2.imshow("My Image", resized_img)
+
+shape = resized_img.shape
+
+# cv2.imshow(f"shape {shape}", resized_img)
+
 cv2.waitKey(0)
